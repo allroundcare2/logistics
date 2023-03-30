8077c52f730e7c2f96469b55f9815b2d9e6fd549
valike23 <63abed62347fae5346839070@bots.bitbucket.org>
2023-03-30 21:20:19 +0100

    fix url2

--- src/Model/public.ts
+++ src/Model/public.ts
@@ -2,7 +2,7 @@
 
 const {  NODE_ENV } = process.env;
 const local = 'http://localhost:4000/';
-const server = 'http://api.allroundcare.ng/';
+const server = 'http://api.allroundcare.org/';
 
 export const url = NODE_ENV == 'development' ? local : server;
 

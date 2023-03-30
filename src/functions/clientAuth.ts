bba1b0444d67fe13434bd1bb07400bb7f349307a
valike23 <63abed62347fae5346839070@bots.bitbucket.org>
2023-03-30 21:20:06 +0100

    fix the url

--- src/functions/clientAuth.ts
+++ src/functions/clientAuth.ts
@@ -22,7 +22,7 @@ export function registration_status(){
 }
 
 export function  getUrl(){
-   return location.hostname == 'localhost'? 'http://localhost:4000' : 'https://api.allroundcare.ng';
+   return location.hostname == 'localhost'? 'http://localhost:4000' : 'https://api.allroundcare.org';
 }
 
 function deg2rad(deg: number) {

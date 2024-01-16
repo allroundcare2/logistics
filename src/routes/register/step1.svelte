bc6cb9e390e62a00a24e547d3a311ec6d1f00f1c
valike23 <63abed62347fae5346839070@bots.bitbucket.org>
2024-01-16 19:52:37 +0100

    Update error message on user creation failure

--- src/routes/register/step1.svelte
+++ src/routes/register/step1.svelte
@@ -51,9 +51,10 @@
             }
         } catch (error) {
             console.log(error);
+            
             let resp = await Swal.fire({
                 title: "user was not created. ",
-                text: "something went wrong , we are aware of it and are on it. Please try again later",
+                text: error.response.data.error,
                 icon: "error",
                 timer: 6000,
             });
